8abd0220cb1ac663cd0f8dd3c813e67ea945e9db
cmcc@macbookair <lansiz@qq.com>
2017-03-09 11:59:33 +0800

    ==

--- fixed_points_of_synapses.py
+++ fixed_points_of_synapses.py
@@ -1,10 +1,12 @@
 # !/usr/bin/env python
 #  -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
 
 from neuron import Brain, Connections, Stimuli
 
 b = Brain(5)
-conns = Connections.grow_on(b)
+conns = Connections.grow_on(b, step=0.00001)
 Stimuli.on(b)
-b.fire(22 * 10 ** 4)
+b.fire(40 * 10 ** 4)
 conns.plot_data()
+plt.savefig('fixed_points_of_synapses.png')
